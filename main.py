653fd597ceeba21a4a9c3be63db348aaddc90f1e
hectoraliagam <aliagamdnhectorgbl@gmail.com>
2024-07-18 20:54:49 -0500

    elimine circulo porque causaba muchos problemas xd, añadir cuadricula y plataformas mas fisicas, asi qeu tambien se elimino el poder moverse con las flechas 'w' y 's', espacio para saltar y si te caes, no retornas xddd

--- main.py
+++ main.py
@@ -8,37 +8,50 @@ info = pg.display.Info()
 WIDTH = info.current_w
 HEIGHT = info.current_h
 SPEED = 10
+GRAVITY = 1
+JUMP_STRENGTH = 20
+GRID_SIZE = 50
 
 class Rectangulo:
     def __init__(self, x, y, width, height, color, border):
         self.rect = pg.Rect(x, y, width, height)
         self.color = color
         self.border = border
+        self.vel_y = 0
     def draw(self, surface):
         pg.draw.rect(surface, self.color, self.rect, self.border)
     def move(self, dx, dy):
         self.rect.x += dx
         self.rect.y += dy
+    def aply_gravity(self):
+        self.vel_y += GRAVITY
+        self.rect.y += self.vel_y
+    def jump(self):
+        self.vel_y = -JUMP_STRENGTH
 
-class Circulo:
-    def __init__(self, x, y, radius, color, border):
-        self.x = x
-        self.y = y
-        self.radius = radius
+class Cuadricula:
+    def __init__(self, grid_size, color):
+        self.grid_size = grid_size
         self.color = color
-        self.border = border
     def draw(self, surface):
-        pg.draw.circle(surface, self.color, (self.x, self.y), self.radius, self.border)
-    def move(self, dx, dy):
-        self.x += dx
-        self.y += dy
+        for x in range(0, WIDTH, self.grid_size):
+            pg.draw.line(surface, self.color, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, self.grid_size):
+            pg.draw.line(surface, self.color, (0, y), (WIDTH, y))
+
+def check_collision(rect, platform):
+    if rect.rect.colliderect(platform.rect) and rect.vel_y > 0:
+        rect.rect.bottom = platform.rect.top
+        rect.vel_y = 0
 
 window = pg.display.set_mode((WIDTH, HEIGHT), pg.FULLSCREEN)
 pg.display.set_caption('ProjectXY')
-window.fill(Azul_Púrpura)
 
-cuadrado = Rectangulo(300, 300, 40, 20, Rojo_Naranja, 3)
-circulo = Circulo(150, 150, 20, Verde_Bosque, 3)
+window.fill(Blanco)
+
+platform = Rectangulo(WIDTH//2 - 100, HEIGHT - 50, 200, 20, Azul_Acero, 0)
+cuadrado = Rectangulo(WIDTH//2, HEIGHT - 70 - 20, 40, 20, Rojo_Naranja, 3)
+cuadricula = Cuadricula(GRID_SIZE, Negro)
 
 clock = pg.time.Clock()
 
@@ -51,6 +64,9 @@ while True:
             if event.key == pg.K_ESCAPE:
                 pg.quit()
                 sys.exit()
+            elif event.key == pg.K_SPACE:
+                if cuadrado.vel_y == 0:
+                    cuadrado.jump()
         
     keys = pg.key.get_pressed()
 
@@ -59,26 +75,14 @@ while True:
         dx = -SPEED
     if keys[pg.K_d] and cuadrado.rect.right < WIDTH:
         dx = SPEED
-    if keys[pg.K_w] and cuadrado.rect.top > 0:
-        dy = -SPEED
-    if keys[pg.K_s] and cuadrado.rect.bottom < HEIGHT:
-        dy = SPEED
     cuadrado.move(dx, dy)
+    cuadrado.aply_gravity()
+    check_collision(cuadrado, platform)
 
-    dx, dy = 0, 0
-    if keys[pg.K_LEFT] and circulo.x - circulo.radius > 0:
-        dx = -SPEED
-    if keys[pg.K_RIGHT] and circulo.x + circulo.radius < WIDTH:
-        dx = SPEED
-    if keys[pg.K_UP] and circulo.y - circulo.radius > 0:
-        dy = -SPEED
-    if keys[pg.K_DOWN] and circulo.y + circulo.radius < HEIGHT:
-        dy = SPEED
-    circulo.move(dx, dy)
-
-    window.fill(Azul_Púrpura)
+    window.fill(Blanco)
+    cuadricula.draw(window)
+    platform.draw(window)
     cuadrado.draw(window)
-    circulo.draw(window)
 
     pg.display.flip()
     clock.tick(60)
